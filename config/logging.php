3c0b9d72ffcdf561e2c19e54bd839c64b1b2e993
raul0103 <rshakurov95@mail.ru>
2023-06-06 01:48:43 +0600

    Изменил файл логирования

--- config/logging.php
+++ config/logging.php
@@ -69,7 +69,8 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 14,
+            'days' => 7,
+            'permission' => 0664,
             'replace_placeholders' => true,
         ],
 
@@ -89,7 +90,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
