6114ab70551bea54c2712d0f2f61ffeb157c532c
raul0103 <rshakurov95@mail.ru>
2023-06-13 00:16:31 +0600

    commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/app.sass', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.sass',
+                'resources/js/app.js',
+                'resources/js/pages/parser.js',
+                'resources/js/pages/settings.js'
+            ],
             refresh: true,
         }),
     ],
