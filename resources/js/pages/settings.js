136e219d5b8f6b5870d4d5c731744721a941a635
raul0103 <rshakurov95@mail.ru>
2023-06-17 17:09:55 +0600

    Исправил удаление апи ключей

--- resources/js/pages/settings.js
+++ resources/js/pages/settings.js
@@ -3,27 +3,29 @@
  * 
  * [data-delete-api-key] - DOM кнопка для удаления с ID удаляемого объекта
  */
-document.querySelector('[data-delete-api-key]')?.addEventListener('click', async e => {
-    e.preventDefault()
+document.querySelectorAll('[data-delete-api-key]').forEach(btn => {
+    btn.addEventListener('click', async e => {
+        e.preventDefault()
 
-    const api_key_id = e.target.dataset.deleteApiKey
+        const api_key_id = e.target.dataset.deleteApiKey
 
-    if (confirm('Подтвердите удаление')) {
-        try {
-            const response = await fetch(`/settings/gpt-api-keys/${api_key_id}`, {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json",
-                    'X-CSRF-TOKEN': CSRF
-                },
-            });
+        if (confirm('Подтвердите удаление')) {
+            try {
+                const response = await fetch(`/settings/gpt-api-keys/${api_key_id}`, {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json",
+                        'X-CSRF-TOKEN': CSRF
+                    },
+                });
 
-            const result = await response.json();
-            if (result) {
-                location.reload()
+                const result = await response.json();
+                if (result) {
+                    location.reload()
+                }
+            } catch (error) {
+                console.error(error);
             }
-        } catch (error) {
-            console.error(error);
         }
-    }
+    })
 })
